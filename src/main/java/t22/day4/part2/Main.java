e992633993f312b3af3435fc3656cfe2f74838df
Vasant <vasant.techie@gmail.com>
2022-12-04 11:20:27 +0530

    added day 4

---
+++ src/main/java/t22/day4/part2/Main.java
@@ -0,0 +1,47 @@
+package t22.day4.part2;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        main.process("src/main/resources/day4");
+    }
+
+    private void process(String path) throws IOException {
+        long total = 0l;
+        List<String> lines = Files.lines(Paths.get(path)).collect(Collectors.toList());
+        for(String line: lines) {
+
+            String[] pairs = line.split(",");
+            String[] firstPair = pairs[0].split("-");
+            String[] secondPair = pairs[1].split("-");
+
+            int firstPairStartNum = Integer.parseInt(firstPair[0].trim());
+            int firstPairEndNum = Integer.parseInt(firstPair[1].trim());
+            int secondPairStartNum = Integer.parseInt(secondPair[0].trim());
+            int secondPairEndNum = Integer.parseInt(secondPair[1].trim());
+
+            boolean res = isOverlap(firstPairStartNum, firstPairEndNum, secondPairStartNum, secondPairEndNum);
+
+            if(res)
+                total += 1;
+        }
+
+        System.out.println("Total: " + total);
+    }
+
+    private boolean isOverlap(int firstPairStartNum, int firstPairEndNum, int secondPairStartNum, int secondPairEndNum) {
+        if(firstPairStartNum < secondPairStartNum && firstPairEndNum < secondPairStartNum) {
+            return false;
+        } else if(secondPairStartNum < firstPairStartNum && secondPairEndNum < firstPairStartNum) {
+            return false;
+        }
+
+        return true;
+    }
+}
