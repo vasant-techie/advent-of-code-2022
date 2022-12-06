daa3cd8b618948d87dbf65e7e8fdca1602437a82
Vasant <vasant.techie@gmail.com>
2022-12-06 19:34:48 +0530

    day 6

---
+++ src/main/java/t22/day6/part2/Main.java
@@ -0,0 +1,38 @@
+package t22.day6.part2;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        int index = main.process("src/main/resources/day6");
+        System.out.println("Result: " + (index + 1));
+    }
+
+    private int process(String path) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get(path));
+        for(String sample: lines) {
+            Set<Integer> coll = new HashSet<>();
+            for(int c=0; c<sample.length(); c++) {
+                for (int i = c; i < sample.length(); i++) {
+                    int ch = sample.codePointAt(i);
+                    boolean res = coll.add(ch);
+                    if (coll.size() == 14 && res == true) {
+                        return i;
+                    } else if (coll.size() < 14 && !res) {
+                        coll.clear();
+                        break;
+                    }
+                }
+            }
+        }
+        System.err.println("### ABNORMAL ###");
+        return -1;
+    }
+}
